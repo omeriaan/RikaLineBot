060c99c2e5dd1a719a501b807e07bcc221782dea
Omeriaan <tennessee.suzuki@gmail.com>
2017-04-30 21:49:32 +0900

    commit

--- bot/urls.py
+++ bot/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ViewSet.as_view()),
+    url(r'^callback', views.ViewSet.as_view()),
 ]
--- rika_line_bot/urls.py
+++ rika_line_bot/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^callback/', include('bot.urls')),
+    url(r'^bot/', include('bot.urls')),
 ]
